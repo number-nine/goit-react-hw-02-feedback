3e1178d339c2953ce698d00a095aa47a5e9c9f00
number-nine <sergii.pokhylko@gmail.com>
2023-05-09 11:44:58 +0300

    initial

--- src/components/Transaction/Transaction.styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-
-export const TransactionCell = styled.td`
-  border: 1px solid #5a5a5a;
-  padding: 8px;
-  border-spacing: 0;
-`;
-
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App/App';
+import { App } from 'components/App';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    HW2-1
     <App />
   </React.StrictMode>
 );
